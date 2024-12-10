b3054db73560130332eede1b9f4d4a9f3d1536c1
hellboy84 <8voldenuit4@gmail.com>
2024-12-10 18:35:03 +0900

    Update 20250220_isbnvolcheck.R

--- 20250220_isbnvolcheck.R
+++ 20250220_isbnvolcheck.R
@@ -6,13 +6,16 @@ library(dplyr)
 df <- read_csv("isbnvolcheck.csv", locale = locale(encoding = "UTF-8"))
 
 # 空白とNAを「巻次」の一つのカテゴリーとして扱うために空白をNAに置き換える
+# データの表記揺れをなくすため
 df$`巻次`[df$`巻次` == ""] <- NA
+
 # ISBNが空白（""）や欠損値（NA）の行を除外する
+# ISBNがないものが一つの資料群として扱われないように
 df_clean <- df %>%
   filter(!is.na(`ISBN`) & `ISBN` != "")
 
 # ISBNごとに巻次に異なるデータがあるレコードを抽出
-# n_distinctの数値列を作成
+# n_distinctの数値列を作成→その後の処理でユニークパターン数で作業を区別するため
 result <- df_clean %>%
   group_by(`ISBN`) %>%
   mutate(distinct_vol_count = n_distinct(`巻次`, na.rm = FALSE)) %>%
